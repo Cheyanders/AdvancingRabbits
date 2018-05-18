8edfaed39d04786419a00580e26deaad19857967
Cheyanders <39174596+Cheyanders@users.noreply.github.com>
2018-05-18 10:15:46 +1000

    Commit for Advancing Rabbits

---
+++ Advancing Rabbits/Assets/Scripts/RabbitMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = transform.position + new Vector3(0, 0, 01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Advancing Rabbits/Assets/Scripts/TriggerPlaySound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlaySound : MonoBehaviour {
+
+	public AudioClip clip;
+	public float interactDelay = 0;
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.tag == "Player") {
+
+			Invoke ("PlaySound", interactDelay);
+		}
+	}
+
+	private void PlaySound()
+	{
+		AudioSource.PlayClipAtPoint (clip, transform.position);
+	}
+
+	void Update () {
+		
+	}
+}
